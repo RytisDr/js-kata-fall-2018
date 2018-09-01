24ec680aa1d2a84add5e0cb4d2d51a38a0e36352
RytisDr <r.drazdauskas@gmail.com>
2018-09-01 20:31:16 +0200

    Finishes optional challenge too

--- w1d5 - concat and pad/mysolution/concat.js
+++ w1d5 - concat and pad/mysolution/concat.js
@@ -1,14 +1,17 @@
 "use strict";
 function concatNPad(string, number) {
-  let numberString = number.toString();
-  if (numberString.length < 2) {
-    numberString = numberString.padStart(2, "0");
-  }
   let allHashtags = string.substring(
     string.indexOf("#"),
     string.lastIndexOf("#") + 1
   );
+  let numberString = number.toString();
+  if (numberString.length < 2) {
+    numberString = numberString.padStart(2, "0");
+  }
+  if (numberString.length < allHashtags.length) {
+    numberString = numberString.padStart(allHashtags.length, "0");
+  }
   let newString = string.replace(allHashtags, numberString);
   return newString;
 }
-console.log(concatNPad("image#.jpg", 1));
+console.log(concatNPad("image###.png", 3));
