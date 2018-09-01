228c18542dba4ded99cda712ca61253313dca8cb
RytisDr <r.drazdauskas@gmail.com>
2018-09-01 23:20:45 +0200

    Fetches tweets with specific hashtags

--- w1d3 - tweets/mysolution/script.js
+++ w1d3 - tweets/mysolution/script.js
@@ -1,9 +1,11 @@
 "use strict";
+let urlParams = new URLSearchParams(window.location.search);
+let hashtag = urlParams.get("hashtag");
 const template = document.querySelector(".tweetTemp").content;
-const tweetCount = 10;
-let hashtag;
+let tweetCount = 50;
+
 const tweetBox = document.querySelector(".tweetSection");
-function fetchTweets(hashtag) {
+function fetchTweets() {
   let endP = "https://kea-alt-del.dk/twitter/api/?_embed&count=" + tweetCount;
   if (hashtag) {
     endP =
@@ -25,20 +27,14 @@ function showSingleTweet(aTweet) {
   let clone = template.cloneNode(true);
   clone.querySelector(".time").textContent = aTweet.created_at;
   clone.querySelector(".tweetText").textContent = aTweet.text;
-  /* clone.querySelector(".hashtags").textContent */
+
   aTweet.entities.hashtags.forEach(function(hashtagInTweet) {
     let a = document.createElement("a");
     a.textContent = hashtagInTweet.text;
-
+    a.href = "?hashtag=" + hashtagInTweet.text;
     clone.querySelector(".hashtags").appendChild(a);
-    a.addEventListener("click", function() {
-      hashtag = hashtagInTweet.text;
-      console.log(hashtag);
-      fetchTweets(hashtag);
-    });
-    /* let hashtags = (clone.querySelector(".hashtags").textContent =
-      hashtag.text); */
   });
   tweetBox.appendChild(clone);
 }
+
 fetchTweets();
