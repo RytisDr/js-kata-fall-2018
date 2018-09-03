23aa9d4dba60d62b2510d18e7aa205407cbb8a36
RytisDr <r.drazdauskas@gmail.com>
2018-09-03 21:18:16 +0200

    Sets up a function that finds the correct element by id.

--- w2d1 - listMethods/mysolution/script.js
+++ w2d1 - listMethods/mysolution/script.js
@@ -1,11 +1,18 @@
 "use strict";
 
-function displayElement( element ) {
-    console.log( element.textContent );
+function displayElement(element) {
+  console.log(element.textContent);
 }
 
 listMethods("modify");
-listMethods("info");
-listMethods("newarray");
+/* listMethods("info");
+listMethods("newarray"); */
 
+function listMethods(idParam) {
+  let foundElement = document.getElementById(idParam);
+  if (foundElement.id == idParam) {
+    let liItems = foundElement.querySelectorAll("ul li");
+    console.log(liItems);
+  }
+}
 // TODO: Create listMethods function
