a6b7101b46ea0bb70e3c4859b42414968a421858
RytisDr <r.drazdauskas@gmail.com>
2018-09-04 16:31:21 +0200

    Adds bulletpoints in console.log, first optional

--- w2d1 - listMethods/mysolution/script.js
+++ w2d1 - listMethods/mysolution/script.js
@@ -1,21 +1,23 @@
 "use strict";
 
 function displayElement(element) {
-  console.log(element);
+  //shows up with bullet points as optional
+  console.log("\u2022" + element.innerHTML);
   /*   for (let i = 0; i < element.length; i++) {
     //console.log(element[i]);
   } */
 }
 
 listMethods("modify");
-/* listMethods("info");
-listMethods("newarray"); */
+listMethods("info");
+listMethods("newarray");
 
 function listMethods(idParam) {
   let foundElement = document.getElementById(idParam);
   if (foundElement.id == idParam) {
-    let liItemsFound = foundElement.querySelectorAll("ul li");
+    let liItemsFound = Array.from(foundElement.querySelectorAll("ul li"));
     liItemsFound.forEach(displayElement);
+    //displayElement();
   }
 }
 
