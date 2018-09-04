73a094f990318e7c405259163bc7deccb35e9c46
RytisDr <r.drazdauskas@gmail.com>
2018-09-04 15:37:45 +0200

    Finishes main task with -for- loop.

--- w2d1 - listMethods/mysolution/script.js
+++ w2d1 - listMethods/mysolution/script.js
@@ -1,18 +1,21 @@
 "use strict";
 
 function displayElement(element) {
-  console.log(element.textContent);
+  for (let i = 0; i < element.length; i++) {
+    console.log(element[i]);
+  }
 }
 
 listMethods("modify");
-/* listMethods("info");
-listMethods("newarray"); */
+listMethods("info");
+listMethods("newarray");
 
 function listMethods(idParam) {
   let foundElement = document.getElementById(idParam);
   if (foundElement.id == idParam) {
-    let liItems = foundElement.querySelectorAll("ul li");
-    console.log(liItems);
+    let liItemsFound = foundElement.querySelectorAll("ul li");
+    displayElement(liItemsFound);
   }
 }
+
 // TODO: Create listMethods function
