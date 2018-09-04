dee706bafb48ff2464ac797d00da5f3af8810aa3
RytisDr <r.drazdauskas@gmail.com>
2018-09-04 15:43:55 +0200

    Finishes main task with -forEach- loop

--- w2d1 - listMethods/mysolution/script.js
+++ w2d1 - listMethods/mysolution/script.js
@@ -1,20 +1,21 @@
 "use strict";
 
 function displayElement(element) {
-  for (let i = 0; i < element.length; i++) {
-    console.log(element[i]);
-  }
+  console.log(element);
+  /*   for (let i = 0; i < element.length; i++) {
+    //console.log(element[i]);
+  } */
 }
 
 listMethods("modify");
-listMethods("info");
-listMethods("newarray");
+/* listMethods("info");
+listMethods("newarray"); */
 
 function listMethods(idParam) {
   let foundElement = document.getElementById(idParam);
   if (foundElement.id == idParam) {
     let liItemsFound = foundElement.querySelectorAll("ul li");
-    displayElement(liItemsFound);
+    liItemsFound.forEach(displayElement);
   }
 }
 
