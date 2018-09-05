a10fe467c2822506b8645506db29ea3b63bbd496
RytisDr <r.drazdauskas@gmail.com>
2018-09-05 20:21:51 +0200

    Now function takes 2 arguments, finishes optional objectives

--- w2d3 - delete the bad review/mysolution/main.js
+++ w2d3 - delete the bad review/mysolution/main.js
@@ -1,9 +1,22 @@
 "use strict";
-const allReviews = document.querySelectorAll("ul li");
+//CORE FUNCTION
+/*const allReviews = document.querySelectorAll("ul li");
 window.addEventListener("DOMContentLoaded", allReviews.forEach(removeBadOnes));
 
 function removeBadOnes(review) {
   if (review.querySelector("span").textContent < 8) {
     review.remove();
   }
+}
+ */
+//FUNCTION WITH 2 ARGUMENTS
+window.addEventListener("DOMContentLoaded", removeBadOnes("li", 7));
+
+function removeBadOnes(selector, minScore) {
+  let allSelectedReviews = document.querySelectorAll(selector);
+  allSelectedReviews.forEach(function(review) {
+    if (review.querySelector("span").textContent < minScore) {
+      review.remove();
+    }
+  });
 }
