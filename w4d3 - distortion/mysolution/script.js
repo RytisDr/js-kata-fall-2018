111fb074bf2933c645c1a89d24b3ee514f69377d
RytisDr <r.drazdauskas@gmail.com>
2018-09-21 21:46:02 +0200

    Animates the changes in queue number

--- w4d3 - distortion/mysolution/script.js
+++ w4d3 - distortion/mysolution/script.js
@@ -1,31 +1,38 @@
 "use strict";
 
 window.addEventListener("DOMContentLoaded", init);
-let template = document.querySelector("template").content;
+
+function init() {
+  //creates 25 divs with transparent background
+  appendDivs();
+  //begin the loop
+  fetchEndP();
+}
+
 function fetchEndP() {
   let endP = "https://kea-alt-del.dk/kata-distortion/";
   fetch(endP)
     .then(e => e.json())
-    .then(appendDivs);
+    .then(colorDivs);
 }
-function appendDivs(data) {
-  let queueNumber = data.inQueue;
-  for (let i = 0; i < queueNumber; i++) {
-    let clone = template.cloneNode(true);
-    let circle = document.createElement("div");
-    clone.appendChild(circle);
-    document.querySelector("main").appendChild(clone);
+function appendDivs() {
+  for (let a = 0; a < 25; a++) {
+    let transpDiv = document.createElement("div");
+    document.querySelector("main").appendChild(transpDiv);
   }
 }
-function removeDivs() {
-  while (document.querySelector("main").hasChildNodes()) {
-    document
-      .querySelector("main")
-      .removeChild(document.querySelector("main").firstChild);
+function colorDivs(data) {
+  let peopleInQueue = data.inQueue;
+  document.querySelector("h2 span").textContent = peopleInQueue;
+  let allDivs = document.querySelectorAll("main div");
+  for (let b = 0; b < peopleInQueue; b++) {
+    allDivs[b].style.backgroundColor = `hsl(${Math.random() * 360}, 50%, 40%)`;
+    allDivs[b].style.transitionDelay = `${b / 6}s`;
   }
-}
-function init() {
-  fetchEndP();
-  setInterval(removeDivs, 10000);
-  setInterval(fetchEndP, 10000);
+  setTimeout(removeColor, 10000);
+  function removeColor() {
+    allDivs.forEach(e => (e.style.backgroundColor = "transparent"));
+    ///fetch again
+    fetchEndP();
+  }
 }
