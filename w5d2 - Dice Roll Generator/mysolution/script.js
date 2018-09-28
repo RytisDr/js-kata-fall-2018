00a1e0f5ef000a36c988186f9d632591fff10a21
RytisDr <r.drazdauskas@gmail.com>
2018-09-29 01:49:17 +0200

    Implements Polart area char via chart js to show the distribu.tion of rolled numbers

--- w5d2 - Dice Roll Generator/mysolution/script.js
+++ w5d2 - Dice Roll Generator/mysolution/script.js
@@ -1,5 +1,4 @@
 "use strict";
-/* var myChart = new Chart(ctx, {...}); */
 import Chart from "chart.js";
 const values = {
   totalRolls: 0,
@@ -11,24 +10,52 @@ const values = {
   6: 0
 };
 window.addEventListener("DOMContentLoaded", init);
-
 function init() {
-  let numberOfRolls = 700000;
+  let numberOfRolls = 500;
   values.totalRolls = numberOfRolls;
-
   for (let i = 0; i < numberOfRolls; i++) {
     let roll = Math.ceil(Math.random() * 6);
-
-    assign(roll);
-    //console.log(roll);
-    //for (let a = 0; a < 6; a++) {
-
-    //let numberArr = Array.from(values[a]);
-    //roll.push(numberArr);
+    createAndAssign(roll);
   }
-  console.table(values);
+  createChart();
 }
-//}
-function assign(roll) {
+function createAndAssign(roll) {
   values[roll]++;
+}
+function createChart() {
+  let myChart = document.querySelector("#myChart");
+  let data = {
+    datasets: [
+      {
+        data: [
+          values[1],
+          values[2],
+          values[3],
+          values[4],
+          values[5],
+          values[6]
+        ],
+        backgroundColor: [
+          "hsl(40, 50%, 50%)",
+          "hsl(120, 50%, 50%)",
+          "hsl(80, 50%, 50%)",
+          "hsl(160, 50%, 50%)",
+          "hsl(0, 50%, 50%)",
+          "hsl(200, 50%, 50%)"
+        ]
+      }
+    ],
+    labels: [
+      "Rolled 1",
+      "Rolled 2",
+      "Rolled 3",
+      "Rolled 4",
+      "Rolled 5",
+      "Rolled 6"
+    ]
+  };
+  new Chart(myChart, {
+    data: data,
+    type: "polarArea"
+  });
 }
