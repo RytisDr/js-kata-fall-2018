35b81ca3eb5bd0c0fca4c028ae87d8b35043dd5a
RytisDr <r.drazdauskas@gmail.com>
2018-10-18 18:41:41 +0200

    Switches buttons, displays them block, function stops switching divs when there are no more, removes event listener.

--- w8d3 - if satan was a web developer part x/mysolution/script.js
+++ w8d3 - if satan was a web developer part x/mysolution/script.js
@@ -1,19 +1,38 @@
 "use strict";
 //https://i.imgur.com/iCycnne.gif
+
+//counter
 let count = 0;
 window.addEventListener("DOMContentLoaded", init);
 function init() {
+  //interval time
+  let intervalTiming = 100;
+  //first div first
   let divNumber = 1;
+  //set interval, pass div number (1) as an argument
   let interval = setInterval(function() {
     counter(divNumber);
-  }, 100);
+  }, intervalTiming);
+  //my set button
   let setBtn = document.querySelector("#set");
-  setBtn.addEventListener("click", function() {
+  //my reset button
+  let resetBtn = document.querySelector("#reset");
+  //upon set button click
+  setBtn.addEventListener("click", function switchDiv() {
+    //clear initial interval
     clearInterval(interval);
-    divNumber++;
-    interval = setInterval(function() {
-      counter(divNumber);
-    }, 100);
+    //if there are divs left to switch...
+    if (divNumber < document.querySelectorAll("body div").length) {
+      //...increment div number...
+      divNumber++;
+      //...and reset interval (with new div number)
+      interval = setInterval(function() {
+        counter(divNumber);
+      }, intervalTiming);
+    } else {
+      //this removal is not neccesary, but lets clean up
+      setBtn.removeEventListener("click", switchDiv);
+    }
   });
 }
 
