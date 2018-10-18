358a96ed5eea3c537e5f5ec6d93862c92016d9bf
RytisDr <r.drazdauskas@gmail.com>
2018-10-18 18:11:07 +0200

    Adds divs, h2's, creates functions to increment number, append it to h2, adds event listener to button (switches h2). Still needs to reset & stop switching at the end.

--- w8d3 - if satan was a web developer part x/mysolution/script.js
+++ w8d3 - if satan was a web developer part x/mysolution/script.js
@@ -1,2 +1,28 @@
 "use strict";
-//https://imgur.com/gallery/qA4Bu
+//https://i.imgur.com/iCycnne.gif
+let count = 0;
+window.addEventListener("DOMContentLoaded", init);
+function init() {
+  let divNumber = 1;
+  let interval = setInterval(function() {
+    counter(divNumber);
+  }, 100);
+  let setBtn = document.querySelector("#set");
+  setBtn.addEventListener("click", function() {
+    clearInterval(interval);
+    divNumber++;
+    interval = setInterval(function() {
+      counter(divNumber);
+    }, 100);
+  });
+}
+
+function counter(divNumber) {
+  count++;
+  if (count == 10) {
+    count = 0;
+  }
+  document.querySelector(
+    `div:nth-of-type(${divNumber}) h2`
+  ).textContent = count;
+}
