4f710224a2aaf398bd780d310518b466913574fe
RytisDr <r.drazdauskas@gmail.com>
2018-10-21 21:20:58 +0200

    Removes validity signs, but adds focus change on correct input on expiry date.

--- w8d4 - payment form/mysolution/script.js
+++ w8d4 - payment form/mysolution/script.js
@@ -2,11 +2,12 @@
 document.querySelectorAll("input").forEach(input => {
   if (input.type == "number") {
     input.addEventListener("input", function() {
-      if (input.checkValidity() && input.value.length == 2) {
-        if (input.nextElementSibling.tagName != "input") {
-          input.nextElementSibling.nextElementSibling.focus();
-        }
-        if (input.nextElementSibling == null) {
+      if (
+        input.checkValidity() &&
+        input.value.length == 2 &&
+        (input.name == "expiration-month" || input.name == "expiration-year")
+      ) {
+        if (!input.nextElementSibling) {
           input.parentElement.nextElementSibling.focus();
         } else {
           input.nextElementSibling.focus();
