0db77d7dc6d57a00aa3ebf2c45bd24531e74b398
RytisDr <r.drazdauskas@gmail.com>
2018-10-21 19:15:56 +0200

    Adds validation spans, needs improvement of swtichching focus input fields.

--- w8d4 - payment form/mysolution/script.js
+++ w8d4 - payment form/mysolution/script.js
@@ -1,6 +1,17 @@
 "use strict";
 document.querySelectorAll("input").forEach(input => {
-  if (input.focus() && input.checkValidity()) {
-    input.nextElementSibling.focus();
+  if (input.type == "number") {
+    input.addEventListener("input", function() {
+      if (input.checkValidity() && input.value.length == 2) {
+        if (input.nextElementSibling.tagName != "input") {
+          input.nextElementSibling.nextElementSibling.focus();
+        }
+        if (input.nextElementSibling == null) {
+          input.parentElement.nextElementSibling.focus();
+        } else {
+          input.nextElementSibling.focus();
+        }
+      }
+    });
   }
 });
